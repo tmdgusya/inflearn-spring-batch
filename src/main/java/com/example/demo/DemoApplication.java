1d5ba7620b93ffbdf5c34dbf3dde72758073a5ae
roach <tmdgusya@naver.com>
2021-12-12 00:34:37 +0900

    EnableBatchProcessing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 스프링 배치가 실행되기 위해 선언되어야 하는 어노테이션
+ */
+@EnableBatchProcessing
 @SpringBootApplication
 public class DemoApplication {
 
