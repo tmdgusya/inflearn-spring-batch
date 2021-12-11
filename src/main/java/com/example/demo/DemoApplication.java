afe13537314e94ce81ce9f3b7d9d914a268ab20e
roach <tmdgusya@naver.com>
2021-12-12 00:42:49 +0900

    add h2 dependency

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 스프링 배치가 실행되기 위해 선언되어야 하는 어노테이션
+ *
+ * 빈으로 등록된 모든 Job 을 검색해서 초기화와 동시에 Job 을 수행하도록 구성됨
  */
 @EnableBatchProcessing
 @SpringBootApplication
