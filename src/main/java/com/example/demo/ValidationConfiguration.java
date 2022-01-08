8c3b2daa1a881a11030c5f897129649e939752a7
roach <tmdgusya@naver.com>
2022-01-08 14:03:34 +0900

    StepBuilder 내부구조 파악용 코드 작성

--- src/main/java/com/example/demo/ValidationConfiguration.java
+++ src/main/java/com/example/demo/ValidationConfiguration.java
@@ -1,14 +1,25 @@
 package com.example.demo;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
@@ -17,17 +28,13 @@ public class ValidationConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    /**
-     * 요게 있으면 그냥 실패해도 다시 시작하려 해도 안됨.
-     * @return
-     */
     @Bean
     public Job batchJob() {
         return this.jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
-                .preventRestart()
-                .incrementer(new CustomJobParametersIncrementer())
+                .next(step3())
                 .build();
     }
 
@@ -43,10 +50,64 @@ public class ValidationConfiguration {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(((stepContribution, chunkContext) -> {
-                    System.out.println("step2 has executed");
-                    return RepeatStatus.FINISHED;
-                })).build();
+                .<String, String>chunk(3)
+                .reader(new ItemReader<String>() {
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        return null;
+                    }
+                })
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String s) throws Exception {
+                        return null;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> list) throws Exception {
+
+                    }
+                }).build();
     }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .partitioner(step1())
+                .gridSize(3)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .job(job())
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .flow(flow())
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean
+    public Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        flowBuilder.start(step2()).end();
+        return flowBuilder.build();
+    }
+
 
 }
