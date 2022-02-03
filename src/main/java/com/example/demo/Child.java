b7c3b75a2dffa6cb314e3cb79da74ba6ac545c4c
roach <tmdgusya@naver.com>
2022-02-04 00:00:31 +0900

    StackOverflow 답변용 클래스 생성

---
+++ src/main/java/com/example/demo/Child.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Child extends Parent {
+
+    public Child() {
+    }
+}
---
+++ src/main/java/com/example/demo/Parent.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Parent {
+
+    private String name;
+    private String age;
+
+    public Parent() {
+    }
+}
